8dcd367ad89c9e133a27db92cca1ccdae22d5cfb
harshGupta090722 <harshgupta0907@gmail.com>
2026-06-19 12:29:13 +0530

    updated

--- backend/test-db.ts
+++ backend/test-db.ts
@@ -5,7 +5,7 @@ import { Payment } from "./models/paymentModel.js";
 dotenv.config();
 
 async function run() {
-  await mongoose.connect(process.env.MONGO_URL);
+  await mongoose.connect(process.env.MONGO_URL || "");
   const payment = await Payment.findOne().sort({ createdAt: -1 });
   console.log(payment);
   process.exit(0);
